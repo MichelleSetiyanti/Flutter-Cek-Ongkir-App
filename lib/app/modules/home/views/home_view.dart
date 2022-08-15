2c267398c75cfc9ee9e440e6b7f42fff97b10940
MichelleSetiyanti <mmichellesetiyanti@gmail.com>
2022-08-15 11:09:08 +0700

    Study Case part 1 done

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -20,10 +22,17 @@ class HomeView extends GetView<HomeController> {
       body: ListView(
         padding: EdgeInsets.all(20),
         children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Text("Tempat Asal",
+                style: TextStyle(
+                  fontSize: 18,
+                )),
+          ),
           DropdownSearch<Province>(
             dropdownDecoratorProps: DropDownDecoratorProps(
               dropdownSearchDecoration: InputDecoration(
-                labelText: "Provinsi",
+                labelText: "Provinsi Asal",
                 contentPadding: EdgeInsets.symmetric(
                   vertical: 10,
                   horizontal: 10,
@@ -60,7 +69,7 @@ class HomeView extends GetView<HomeController> {
           DropdownSearch<City>(
             dropdownDecoratorProps: DropDownDecoratorProps(
               dropdownSearchDecoration: InputDecoration(
-                labelText: "Kota/Kabupaten",
+                labelText: "Kota/Kabupaten Asal",
                 contentPadding: EdgeInsets.symmetric(
                   vertical: 10,
                   horizontal: 10,
@@ -90,6 +99,141 @@ class HomeView extends GetView<HomeController> {
                   City.fromJsonList(response.data["rajaongkir"]["results"]);
               return models;
             },
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Text("Tempat Tujuan",
+                style: TextStyle(
+                  fontSize: 18,
+                )),
+          ),
+          DropdownSearch<Province>(
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "Provinsi Tujuan",
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            onChanged: (value) =>
+                controller.provTujuanId.value = value?.provinceId ?? "0",
+            // dropdownBuilder: (context, selectedItem) => ListTile(
+            //   title: Text(
+            //       selectedItem?["province"].toString() ?? "Belum Pilih data"),
+            // ),
+            popupProps: PopupProps.menu(
+              showSearchBox: true,
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text("${item.province}"),
+              ),
+            ),
+            asyncItems: (String filter) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/province",
+                queryParameters: {"key": "5c1a41d60074756b308ad9635d893c76"},
+              );
+              // print(response.data["rajaongkir"]["results"]);
+              var models =
+                  Province.fromJsonList(response.data["rajaongkir"]["results"]);
+              return models;
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownSearch<City>(
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "Kota/Kabupaten Tujuan",
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            // dropdownBuilder: (context, selectedItem) => ListTile(
+            //   title: Text(
+            //       selectedItem?["province"].toString() ?? "Belum Pilih data"),
+            // ),
+            popupProps: PopupProps.menu(
+              showSearchBox: true,
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text("${item.type} ${item.cityName}"),
+              ),
+            ),
+            onChanged: (value) =>
+                controller.cityTujuanId.value = value?.cityId ?? "0",
+            asyncItems: (String filter) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/city?province=${controller.provTujuanId}",
+                queryParameters: {"key": "5c1a41d60074756b308ad9635d893c76"},
+              );
+              // print(response.data["rajaongkir"]["results"]);
+              var models =
+                  City.fromJsonList(response.data["rajaongkir"]["results"]);
+              return models;
+            },
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Text("Kurir",
+                style: TextStyle(
+                  fontSize: 18,
+                )),
+          ),
+          DropdownSearch<Map<String, dynamic>>(
+            items: [
+              {
+                "code": "jne",
+                "name": "JNE",
+              },
+              {
+                "code": "pos",
+                "name": "POS Indonesia",
+              },
+              {
+                "code": "tiki",
+                "name": "TIKI",
+              }
+            ],
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                labelText: "Pilih Kurir",
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            popupProps: PopupProps.menu(
+              showSearchBox: true,
+              itemBuilder: (context, item, isSelected) => ListTile(
+                title: Text("${item['name']}"),
+              ),
+            ),
+            dropdownBuilder: (context, selectedItem) =>
+                Text("${selectedItem?['name'] ?? "Pilih Kurir"}"),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("CEK ONGKOS KIRIM"),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+                padding: MaterialStateProperty.all(EdgeInsets.all(20))),
           ),
         ],
       ),
